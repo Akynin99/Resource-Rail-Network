0489c5f766ba9c6827739fed2cead455759cf733
Stepan Ustinov <Akynin99@gmail.com>
2025-05-14 19:45:01 +0700

    feat: train config

---
+++ Assets/Scripts/Train/TrainConfig.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace ResourceRailNetwork.Train
+{
+    [CreateAssetMenu(fileName = "TrainConfig", menuName = "ResourceRailNetwork/TrainConfig", order = 0)]
+    public class TrainConfig : ScriptableObject
+    {
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private float speed;
+        [SerializeField] private float miningTime;
+
+        public GameObject Prefab => prefab;
+        public float Speed => speed;
+        public float MiningTime => miningTime;
+    }
+}
