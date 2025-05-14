df7c48b4190bb537ea1f425a012d69b19f3bdcec
Stepan Ustinov <Akynin99@gmail.com>
2025-05-14 19:45:09 +0700

    feat: spawn trains

---
+++ Assets/Scripts/Train/Train.cs
@@ -0,0 +1,22 @@
+﻿using ResourceRailNetwork.Graph;
+using UnityEngine;
+
+namespace ResourceRailNetwork.Train
+{
+    public class Train
+    {
+        public GameObject TrainView;
+        public Node LastNode;
+        public Node NextNode;
+        public Node TargetNode;
+        public float Progress;
+        public TrainState State;
+
+        public enum TrainState
+        {
+            Moving,
+            Mining,
+            Delivering
+        }
+    }
+}
---
+++ Assets/Scripts/Train/TrainController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace ResourceRailNetwork.Train
+{
+    public class TrainController : MonoBehaviour
+    {
+        [SerializeField] private TrainConfig[] trainConfigs;
+
+        private Train[] _trains;
+
+        private void Init()
+        {
+            SpawnTrains();
+        }
+
+        private void SpawnTrains()
+        {
+            _trains = new Train[trainConfigs.Length];
+
+            for (int i = 0; i < _trains.Length; i++)
+            {
+                _trains[i] = SpawnTrain(trainConfigs[i]);
+            }
+        }
+
+        private Train SpawnTrain(TrainConfig config)
+        {
+            GameObject view = Instantiate(config.Prefab, transform);
+            Train train = new Train()
+            {
+                TrainView = view,
+                State = Train.TrainState.Moving
+            };
+            
+            
+            
+            return train;
+        }
+    }
+}
